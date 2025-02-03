992f8e3f78252916165dc3ea64e47a0976248cbb
Kavindu Dissanayaka <dissanayakakavindu100@gmail.com>
2025-02-03 15:20:27 +0530

    added some normal list

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ function index() {
   return (
     <div>
       <h2>hiiii</h2>
+
+      <p> this is my list</p>
+      <li>
+        <ul>d1</ul>
+        <ul>d2</ul>
+        <ul>d3</ul>
+      </li>
     </div>
   )
 }

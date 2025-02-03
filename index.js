ce48c95e02c8f76ab831529298728ea279e6ff6f
Kavindu Dissanayaka <dissanayakakavindu100@gmail.com>
2025-02-03 15:26:47 +0530

    changes the body

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ function index() {
   return (
     <div>
       <h2>hiiii</h2>
+
+      
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+
+  
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+
+    
     </div>
   )
 }
